31bce1e94ff81e1f7ebde61fba61d363329c623b
Dias0406 <dias.shamel@gmail.com>
2026-02-03 19:14:52 +0500

    Dias Shamel: - Implement custom checked exception in edu.aitu.oop3.exceptions - Extend Exception to enforce handling in service/controller layers - Provide constructor to accept descriptive error message

--- src/edu/aitu/oop3/exceptions/TaskWithoutProjectException.java
+++ src/edu/aitu/oop3/exceptions/TaskWithoutProjectException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package edu.aitu.oop3.exceptions;
 
 public class TaskWithoutProjectException extends Exception {
     public TaskWithoutProjectException(String message) { super(message); }
