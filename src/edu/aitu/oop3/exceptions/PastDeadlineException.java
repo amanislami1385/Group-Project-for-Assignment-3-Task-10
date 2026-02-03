8999e56c5b14f81a6007051bc39d8af0ea92809e
Dias0406 <dias.shamel@gmail.com>
2026-02-03 19:13:59 +0500

    Dias Shamel: - Implement custom checked exception in edu.aitu.oop3.exceptions - Extend Exception to enforce handling in service/controller layers - Provide constructor to accept descriptive error message

--- src/edu/aitu/oop3/exceptions/PastDeadlineException.java
+++ src/edu/aitu/oop3/exceptions/PastDeadlineException.java
@@ -1,4 +1,4 @@
-package exceptions;
+package edu.aitu.oop3.exceptions;
 
 public class PastDeadlineException extends Exception {
     public PastDeadlineException(String message) { super(message); }
